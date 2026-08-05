b6637344fe7fc0a8164b4948a84319f97d0c7855
Vigneshselvan .V <vigneshselvan.v@gmail.com>
2026-08-05 18:37:10 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        for(String s:strs){
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String s1  = new String(ch);
+            if(!map.containsKey(s1)){
+                map.put(s1,new ArrayList<>());
+            }
+            map.get(s1).add(s);
+
+        }
+        return new ArrayList<>(map.values());
+    }
+}
