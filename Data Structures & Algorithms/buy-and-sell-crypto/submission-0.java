772cde5efd6790ad5681a3f789a4b7b9dc70621d
Vigneshselvan .V <vigneshselvan.v@gmail.com>
2026-08-13 19:23:15 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy = prices[0];
+        int max = 0;
+        for(int i = 1;i<prices.length;i++){
+            if(buy>prices[i]){
+                buy = prices[i];
+            }
+            else{
+                int cur = prices[i]-buy;
+                max = Math.max(cur,max);
+            }
+        }
+        return max;
+    }
+}
